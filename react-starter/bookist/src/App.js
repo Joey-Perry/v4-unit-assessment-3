4edacd98e10171e8acc37f802ae25a9a1893869e
Joey-Perry <Joeyap84@gmail.com>
2021-06-22 20:02:43 -0500

    Initial commit for bookist app

---
+++ react-starter/bookist/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/Header'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      books: [],
+      shelf: [],
+    }
+  }
+
+  addToShelf(){
+
+  }
+  clearShelf(){
+
+  }
+  filterBooks(){
+
+  }
+  reset(){
+    
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
